2d2e60efbf9eeab8fbe0466f4d91934dd6101d49
cooltoast <mpatel678@gmail.com>
2015-06-02 15:42:16 -0700

    add base app and gitignore

---
+++ app/src/main/java/com/example/mp/gcmtest/Utils.java
@@ -0,0 +1,12 @@
+package com.example.mp.gcmtest;
+
+import android.app.Application;
+
+/**
+ * Created by mp on 6/1/15.
+ */
+public class Utils extends Application {
+    public static String GCMSenderId = "587535125412";
+    public static boolean notificationReceived;
+    public static String notiTitle="",notiType="",notiMsg="",notiUrl="",registrationId = "";
+}
