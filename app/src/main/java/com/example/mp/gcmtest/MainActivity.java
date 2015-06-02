6eb6e9ca6e4f8302d951e775db56e123c2aa8d8a
cooltoast <mpatel678@gmail.com>
2015-06-02 16:32:53 -0700

    add GCMregistrar

--- app/src/main/java/com/example/mp/gcmtest/MainActivity.java
+++ app/src/main/java/com/example/mp/gcmtest/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.mp.gcmtest;
 
+import com.google.android.gcm.GCMRegistrar;
 import com.google.android.glass.media.Sounds;
 import com.google.android.glass.widget.CardBuilder;
 import com.google.android.glass.widget.CardScrollAdapter;
 import com.google.android.glass.widget.CardScrollView;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.util.Log;
 
 /**
  * An {@link Activity} showing a tuggable "Hello World!" card.
@@ -76,6 +81,30 @@ public class MainActivity extends Activity {
             }
         });
         setContentView(mCardScroller);
+
+        GCMRegistrar.checkDevice(this);
+
+        // Make sure the device has the proper dependencies.
+        GCMRegistrar.checkDevice(this);
+        // Make sure the manifest was properly set - comment out this line
+        // while developing the app, then uncomment it when it's ready.
+        GCMRegistrar.checkManifest(this);
+
+        String regId = GCMRegistrar.getRegistrationId(this);
+        if (regId.equals("")) {
+            GCMRegistrar.register(this, Utils.GCMSenderId);
+            regId = GCMRegistrar.getRegistrationId(this);
+        } else {
+            Log.v("", "Already registered: "+regId);
+        }
+
+        /**
+         We set this varible true when we recive notification
+         (this code must be in activity which not showing your applciaiton splash screen).
+         */
+        if (Utils.notificationReceived) {
+            onNotification();
+        }
     }
 
     @Override
@@ -90,6 +119,12 @@ public class MainActivity extends Activity {
         super.onPause();
     }
 
+    @Override
+    protected void onDestroy() {
+        GCMRegistrar.onDestroy(this);
+        super.onDestroy();
+    }
+
     /**
      * Builds a Glass styled "Hello World!" view using the {@link CardBuilder} class.
      */
@@ -98,6 +133,34 @@ public class MainActivity extends Activity {
 
         card.setText(R.string.hello_world);
         return card.getView();
+    }
+
+
+    public void onNotification(){
+        Utils.notificationReceived=false;
+        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
+        wl.acquire();
+
+        AlertDialog.Builder mAlert=new AlertDialog.Builder(this);
+        mAlert.setCancelable(true);
+
+        mAlert.setTitle(Utils.notiTitle);
+        mAlert.setMessage(Utils.notiMsg);
+        mAlert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // TODO Auto-generated method stub
+                dialog.dismiss();
+            }
+        });
+        mAlert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // TODO Auto-generated method stub
+                dialog.dismiss();
+            }
+        });
+
+        mAlert.show();
     }
 
 }
